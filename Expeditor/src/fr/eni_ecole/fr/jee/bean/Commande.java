0f4997a531ebb2d680efeb8a3ee25948c2b1ceb9
paul-adrien <douteaupa@mailoo.org>
2016-07-20 13:20:54 +0200

    z

--- Expeditor/src/fr/eni_ecole/fr/jee/bean/Commande.java
+++ Expeditor/src/fr/eni_ecole/fr/jee/bean/Commande.java
@@ -5,20 +5,15 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="COMMANDE")
 public class Commande implements Serializable {
 	
-	private static final long serialVersionUID = -2056667874072513760L;
-
 	@Id
 	@Column(name="id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -27,64 +22,16 @@ public class Commande implements Serializable {
 	@Column(name="num_commande")
 	private String numCommande;
 	
-	@Column(name="date_creation")
 	private Date dateCreation;
 	
-	@Column(name="poids_total")
 	private int poidsTotal;
 	
-	@Column(name="nom")
 	private String nom;
 	
-	@Column(name="adresse")
 	private String adresse;
 	
-	@ManyToOne(optional=false, fetch=FetchType.EAGER)
-	@JoinColumn(name="etat_commande", referencedColumnName="id")
 	//private EtatCommande etatCommande;
 	
-	@ManyToOne(optional=false, fetch=FetchType.EAGER)
-	@JoinColumn(name="utilisateur", referencedColumnName="id")
 	private Utilisateur utilisateur;
-
-	public Commande() { super(); }
-
-	public Commande(long id, String numCommande, Date dateCreation, int poidsTotal, String nom, String adresse, EtatCommande etatCommande, Utilisateur utilisateur) {
-		this();
-		this.id = id;
-		this.numCommande = numCommande;
-		this.dateCreation = dateCreation;
-		this.poidsTotal = poidsTotal;
-		this.nom = nom;
-		this.adresse = adresse;
-		this.etatCommande = etatCommande;
-		this.utilisateur = utilisateur;
-	}
-
-	public long getId() { return id; }
-	public void setId(long id) { this.id = id; }
-
-	public String getNumCommande() { return numCommande; }
-	public void setNumCommande(String numCommande) { this.numCommande = numCommande; }
-
-	public Date getDateCreation() { return dateCreation; }
-	public void setDateCreation(Date dateCreation) { this.dateCreation = dateCreation; }
-
-	public int getPoidsTotal() { return poidsTotal; }
-	public void setPoidsTotal(int poidsTotal) { this.poidsTotal = poidsTotal; }
-
-	public String getNom() { return nom; }
-	public void setNom(String nom) { this.nom = nom; }
-
-	public String getAdresse() { return adresse; }
-	public void setAdresse(String adresse) { this.adresse = adresse; }
-
-	public EtatCommande getEtatCommande() { return etatCommande; }
-	public void setEtatCommande(EtatCommande etatCommande) { this.etatCommande = etatCommande; }
-
-	public Utilisateur getUtilisateur() { return utilisateur; }
-	public void setUtilisateur(Utilisateur utilisateur) { this.utilisateur = utilisateur; }
-
-	public static long getSerialversionuid() { return serialVersionUID; }
 	
 }
