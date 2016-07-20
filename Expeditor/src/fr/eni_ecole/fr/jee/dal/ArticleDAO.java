02318fdd8b824b73d002f4d0d71795a701a98ed1
Zanguy <maxime.triguero@gmail.com>
2016-07-20 10:17:37 +0200

    Servlet

--- Expeditor/src/fr/eni_ecole/fr/jee/dal/ArticleDAO.java
+++ Expeditor/src/fr/eni_ecole/fr/jee/dal/ArticleDAO.java
@@ -15,18 +15,23 @@ import fr.eni_ecole.fr.jee.util.PersistenceManager;
 public class ArticleDAO {
 	private final static String RECHERCHE_ARTICLE = "select * from ARTICLE";
 
-	public static List<Article> rechercheArticleNonAttribue(Article animateur) {
+	public static Article rechercheArticleNonAttribue() {
 		EntityManager em = PersistenceManager.createEntityManager();
 		EntityTransaction trans = em.getTransaction();
 		trans.begin();
 		
+		
+		Article a = null;
 		List<Article> listeArticle = (List<Article>)em.createQuery(RECHERCHE_ARTICLE);
 		
+		for (Article item : listeArticle) {
+			a = item;
+		}
 		
 		trans.commit();
 		em.close();
 		
-		return listeArticle;
+		return a;
 	}
 	
 }
---
+++ Expeditor/src/fr/eni_ecole/fr/jee/util/Constant.java
@@ -0,0 +1,9 @@
+package fr.eni_ecole.fr.jee.util;
+
+public class Constant {
+
+	
+	public static final String EMPLOYE ="employe";
+	
+	public static final String MANAGER ="manager";
+}
