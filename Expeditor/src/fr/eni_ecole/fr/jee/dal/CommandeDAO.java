78936ef8f85185984d0992187bac167df434f5f2
Administrateur <Administrateur@WINDOWS-JNOT4LP.ad.campus-eni.fr>
2016-07-21 14:13:00 +0200

    juh

--- Expeditor/src/fr/eni_ecole/fr/jee/dal/CommandeDAO.java
+++ Expeditor/src/fr/eni_ecole/fr/jee/dal/CommandeDAO.java
@@ -19,7 +19,7 @@ public class CommandeDAO {
 			+ "ORDER BY dateCreation ASC";
 	
 	public static final String OBTENIR_COMMANDES = 
-			"FROM Commande"
+			"FROM Commande "
 		+   "WHERE etatCommande.id != 3";
 	
 	public static Commande obtenirCommandeEmploye(){
@@ -71,22 +71,23 @@ public class CommandeDAO {
 		return liste;
 	}
 	
-	public List<Stat> obtenirStatistiquesJournalieres(){
+	public static List<Stat> obtenirStatistiquesJournalieres(){
 		List<Stat> liste = new ArrayList<Stat>();
 		EntityManager em = PersistenceManager.createEntityManager();
 		EntityTransaction trans = em.getTransaction();
 		trans.begin();
 		
-		Query req = em.createNativeQuery("SELECT u.prenom + ' ' + UPPER(u.nom) as nomPersonne, COUNT(*) as nbCommandes"
-										+ "FROM COMMANDE c"
-										+ "INNER JOIN UTILISATEUR u ON c.utilisateur = u.id"
-										+ "WHERE etat_commande = 3"
-										+ "AND CONVERT(CHAR(10), c.date_modif, 121) = CONVERT(CHAR(10), GETDATE(), 121)"
+		Query req = em.createNativeQuery("SELECT u.prenom + ' ' + UPPER(u.nom) as nomPersonne, COUNT(*) as nbCommandes "
+										+ "FROM COMMANDE c "
+										+ "INNER JOIN UTILISATEUR u ON c.utilisateur = u.id "
+										+ "WHERE etat_commande = 3 "
+										+ "AND CONVERT(CHAR(10), c.date_modif, 121) = CONVERT(CHAR(10), GETDATE(), 121) "
 										+ "GROUP BY u.prenom + ' ' + UPPER(u.nom);");
 		
 		List list = req.getResultList();
 		
-		
+		//Le getResult renvoie les bonnes lignes.
+		//Il faut juste binder les objets Stats dans la liste.
 		
 		trans.commit();
 		em.close();
