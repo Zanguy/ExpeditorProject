d42aa5fb5a96955bc1c7186c81befb3ab448b02d
Zanguy <maxime.triguero@gmail.com>
2016-07-21 14:33:39 +0200

    ArticleDAO

--- Expeditor/src/fr/eni_ecole/fr/jee/dal/ArticleDAO.java
+++ Expeditor/src/fr/eni_ecole/fr/jee/dal/ArticleDAO.java
@@ -15,23 +15,63 @@ import fr.eni_ecole.fr.jee.util.PersistenceManager;
 public class ArticleDAO {
 	private final static String RECHERCHE_ARTICLE = "select * from ARTICLE";
 
-	public static Article rechercheArticleNonAttribue() {
+	
+	
+	public static List<Article> getAllArticle(){
+		
 		EntityManager em = PersistenceManager.createEntityManager();
 		EntityTransaction trans = em.getTransaction();
 		trans.begin();
 		
 		
-		Article a = null;
 		List<Article> listeArticle = (List<Article>)em.createQuery(RECHERCHE_ARTICLE);
 		
-		for (Article item : listeArticle) {
-			a = item;
-		}
 		
 		trans.commit();
 		em.close();
 		
-		return a;
+		return listeArticle;
+	}
+	
+	public static Article findById(Article a){
+		
+		EntityManager em = PersistenceManager.createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		
+		Article article = (Article)em.find(Article.class, a.getId());
+		
+		trans.commit();
+		em.close();
+		
+		return article;
+	}
+	
+	public static void createArticle(Article a){
+		
+		EntityManager em = PersistenceManager.createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		
+		a.setId(Long.valueOf(-1));
+		em.merge(a);
+		
+		trans.commit();
+		em.close();
+		
+	}
+	
+	public static void updateArticle(Article a){
+		
+		EntityManager em = PersistenceManager.createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		
+		em.merge(a);
+		
+		trans.commit();
+		em.close();
+		
 	}
 	
 }
