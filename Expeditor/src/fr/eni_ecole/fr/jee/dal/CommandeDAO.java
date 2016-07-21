4fdf70781b3dc4f6c865871f4795bb9f6c9abe09
Zanguy <maxime.triguero@gmail.com>
2016-07-21 14:22:44 +0200

    ty

--- Expeditor/src/fr/eni_ecole/fr/jee/dal/CommandeDAO.java
+++ Expeditor/src/fr/eni_ecole/fr/jee/dal/CommandeDAO.java
@@ -1,6 +1,5 @@
 package fr.eni_ecole.fr.jee.dal;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,7 +8,6 @@ import javax.persistence.Query;
 
 import fr.eni_ecole.fr.jee.bean.Commande;
 import fr.eni_ecole.fr.jee.bean.LigneCommande;
-import fr.eni_ecole.fr.jee.bean.Stat;
 import fr.eni_ecole.fr.jee.util.PersistenceManager;
 
 public class CommandeDAO {
@@ -17,11 +15,6 @@ public class CommandeDAO {
 			+ "WHERE etatCommande.id = 1 "
 			+ "AND utilisateur IS NULL "
 			+ "ORDER BY dateCreation ASC";
-	
-	public static final String OBTENIR_COMMANDES = 
-			"FROM Commande "
-		+   "WHERE etatCommande.id != 3";
-	
 	public static Commande obtenirCommandeEmploye(){
 		List<Commande> liste = null;
 		EntityManager em = PersistenceManager.createEntityManager();
@@ -33,6 +26,8 @@ public class CommandeDAO {
 		liste = (List<Commande>)req.getResultList();
 	
 		//List<LigneCommande> test = liste.get(0).getLigneCommandes();
+		
+		
 		
 		trans.commit();
 		em.close();
@@ -53,44 +48,5 @@ public class CommandeDAO {
 		
 		trans.commit();
 		em.close();
-	}
-	
-	public static List<Commande> obtenirCommandes(){
-		List<Commande> liste = new ArrayList<Commande>();
-		EntityManager em = PersistenceManager.createEntityManager();
-		EntityTransaction trans = em.getTransaction();
-		trans.begin();
-		
-		Query req = em.createQuery(OBTENIR_COMMANDES);
-		
-		liste = (List<Commande>)req.getResultList();
-		
-		trans.commit();
-		em.close();
-		
-		return liste;
-	}
-	
-	public List<Stat> obtenirStatistiquesJournalieres(){
-		List<Stat> liste = new ArrayList<Stat>();
-		EntityManager em = PersistenceManager.createEntityManager();
-		EntityTransaction trans = em.getTransaction();
-		trans.begin();
-		
-		Query req = em.createNativeQuery("SELECT u.prenom + ' ' + UPPER(u.nom) as nomPersonne, COUNT(*) as nbCommandes"
-										+ "FROM COMMANDE c"
-										+ "INNER JOIN UTILISATEUR u ON c.utilisateur = u.id"
-										+ "WHERE etat_commande = 3"
-										+ "AND CONVERT(CHAR(10), c.date_modif, 121) = CONVERT(CHAR(10), GETDATE(), 121)"
-										+ "GROUP BY u.prenom + ' ' + UPPER(u.nom);");
-		
-		List list = req.getResultList();
-		
-		
-		
-		trans.commit();
-		em.close();
-		
-		return liste;
-	}
+	}	
 }
